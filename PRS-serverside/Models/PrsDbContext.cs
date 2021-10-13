bc8aed60ae7fe259ed4c08969a41f09d185a4276
Michael Walker <walkerrepair255@yahoo.com>
2021-10-13 09:17:38 -0400

    Add project files.

---
+++ PRS-serverside/Models/PrsDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRS_serverside.Models
+{
+    public class PrsDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Vendor> Vendors { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Request> Requests { get; set; }
+        public virtual DbSet<RequestLine> RequestLines { get; set; }
+        
+        public PrsDbContext(DbContextOptions<PrsDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(e =>
+            {
+                e.HasIndex(p => p.Username).IsUnique(true);
+            });
+            builder.Entity<Vendor>(e =>
+            {
+                e.HasIndex(p => p.Code).IsUnique(true);
+            });
+            builder.Entity<Product>(e =>
+            {
+                e.HasIndex(p => p.PartNbr).IsUnique(true);
+            });
+        }
+    }
+}
---
+++ PRS-serverside/Models/RequestLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PRS_serverside.Models
+{
+    public class RequestLine
+    {
+        
+
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; } = 0;
+
+        public virtual Product Product { get; set; }
+        [JsonIgnore]
+        public virtual Request Request { get; set; }
+    }
+}
